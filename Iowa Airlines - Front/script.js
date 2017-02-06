30fabf1b862cf527cd751cee7e067e51ce230655
Kristine Holland <holland.krissy@gmail.com>
2017-02-06 17:33:49 -0600

    Updates

--- Iowa Airlines - Front/script.js
+++ Iowa Airlines - Front/script.js
@@ -1,8 +1,4 @@
 document.getElementById("foot01").innerHTML =
     '<p>&copy;  ' + new Date().getFullYear() + ' FSE - Team Seven. All rights reserved.</p>';
 
-document.getElementById("nav01").innerHTML = "<ul id='menu'>" 
-+ "<li><a href='index.html'>Home</a></li>" 
-+ "<li><a href='about.html'>About Us</a></li>" 
-+ "<li><a href='map.html'>Map</a></li>" 
-+ "</ul>";
+document.getElementById("nav01").innerHTML = "<ul id='menu'>" + "<li><a href='index.html'>Home</a></li>" + "<li><a href='about.html'>About Us</a></li>" + "<li><a href='map.html'>Map</a></li>" + "<li><a href='plan.html'>Book a Trip</a></li>" + "</ul>";
