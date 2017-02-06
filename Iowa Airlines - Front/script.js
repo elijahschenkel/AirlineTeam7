72827119b9285b8f98edfffa8cfd8f4ba5c2bce2
Kristine Holland <hollandk@users.noreply.github.com>
2017-02-06 11:22:03 -0600

    Starting the front end
    Just starting the front end stuff

---
+++ Iowa Airlines - Front/script.js
@@ -0,0 +1,8 @@
+document.getElementById("foot01").innerHTML =
+    '<p>&copy;  ' + new Date().getFullYear() + ' FSE - Team Seven. All rights reserved.</p>';
+
+document.getElementById("nav01").innerHTML = "<ul id='menu'>" 
++ "<li><a href='index.html'>Home</a></li>" 
++ "<li><a href='about.html'>About Us</a></li>" 
++ "<li><a href='map.html'>Map</a></li>" 
++ "</ul>";
