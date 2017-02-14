5452f6b17b435640b8edb6e788e76b3a3e8bbf81
Kristine Holland <holland.krissy@gmail.com>
2017-02-13 19:05:56 -0600

    Updates
    Added login validation function (script.js) 
    Added login form to index.html and plan.html
    Added redirect function (script.js) for future use -- click on destination and redirect to plan.html (map->plan) -- research how to make map interactive (i.e. clickable)

--- Iowa Airlines - Front/script.js
+++ Iowa Airlines - Front/script.js
@@ -3,3 +3,22 @@ document.getElementById("foot01").innerHTML =
 
 document.getElementById("nav01").innerHTML = "<ul id='menu'>" + "<li><a href='index.html'>Home</a></li>" + "<li><a href='about.html'>About Us</a></li>" + "<li><a href='map.html'>Map</a></li>" + "<li><a href='plan.html'>Book a Trip</a></li>" + "</ul>";
 
+function login_validation() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var attempt = 3;
+
+    if(username == "tester" && password == "1213"){
+        alert("Login successful!");
+        return false;
+    } else {
+        attempt--;
+        alert("Login unsuccessful.You have " +attempt+ " attempts left.");
+    }
+}
+
+//redirect function (map --> book trip?)
+function load(url){
+    location.href = url;
+}
+
