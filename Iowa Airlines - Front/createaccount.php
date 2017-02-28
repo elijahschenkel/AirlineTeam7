faa4d27ffcbcd30fe8e927c914e015e66d1d6a9e
Kristine Holland <holland.krissy@gmail.com>
2017-02-27 19:43:04 -0600

    Upload
    Somehow I broke my mySQL connection -- work in progress

--- Iowa Airlines - Front/createaccount.php
+++ Iowa Airlines - Front/createaccount.php
@@ -9,32 +9,30 @@ $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to My
 $db=mysqli_select_db($con, DB_NAME) or die("Failed to connect to MySQL: " . mysqli_connect_error()); 
 
 function NewUser(mysqli $con) {
-    session_start();
 	$fullname = $_POST['name'];
 	$userName = $_POST['user'];
 	$email = $_POST['email'];
 	$password =  $_POST['pass'];
-	$query = "INSERT INTO userDB (fullname,userName,email,pass) VALUES ('$fullname','$userName','$email','$password')";
-    
-	$data = mysqli_query($query)or die(mysqli_connect_error());
+	$query = "INSERT INTO userName (fullname,userName,pass,email) VALUES ('$fullname','$userName','$password','$email')";
+    $data = mysqli_query($query)or die(mysqli_connect_error());
 	if($data) {
-	echo "YOUR REGISTRATION IS COMPLETED...";
+	   echo "YOUR REGISTRATION IS COMPLETED...";
 	}
 }
 
-/*function SignUp(mysqli $con){
+function SignUp(mysqli $con){
     session_start();
     if(!empty($_POST['user'])) {
 	   $query = mysql_query("SELECT * FROM userName WHERE userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysqli_connect_error());
 
 	   if(!$row = mysql_fetch_array($query) or die(mysqli_connect_error())) {
-		  newuser();
+		  newuser($con);
 	   }
 	   else {
            echo "This account is already registered";
 	   }
     }
-}*/
+}
 
 if(isset($_POST['submit'])) {
 	SignUp($con);
