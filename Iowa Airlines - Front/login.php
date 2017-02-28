4d198600abfa4e05b20ac1c278fb5cdb665bf0bd
Kristine Holland <holland.krissy@gmail.com>
2017-02-27 18:20:25 -0600

    Successfully linked frontend, log-in logic, and user database

--- Iowa Airlines - Front/login.php
+++ Iowa Airlines - Front/login.php
@@ -1,22 +1,24 @@
 <?php 
-define('DB_HOST', 'localhost'); 
+define('DB_HOST', '127.0.0.1'); 
 define('DB_NAME', 'user'); 
 define('DB_USER','root'); 
-define('DB_PASSWORD',''); 
+define('DB_PASSWORD','leonidas'); 
 
 $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysqli_connect_error()); 
 
-$db=mysqli_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysqli_connect_error()); 
+$db=mysqli_select_db($con, DB_NAME) or die("Failed to connect to MySQL: " . mysqli_connect_error()); 
 
 /*$ID = $_POST['user']; 
 $Password = $_POST['pass'];*/ 
 
-function SignIn() { 
+function SignIn(mysqli $con) { 
 	session_start(); //starting the session for user profile page 
 	if(!empty($_POST['user'])) //checking the 'user' name which is from Sign-In.html, is it empty or have some text 
 	{ 
-		$query = mysqli_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysqli_connect_error()); 
+		$query = mysqli_query($con,"SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysqli_connect_error()); 
+        
 		$row = mysqli_fetch_array($query) or die(mysqli_connect_error()); 
+        
 		if(!empty($row['userName']) AND !empty($row['pass'])) 
 			{ 
 				$_SESSION['userName'] = $row['pass']; 
@@ -28,7 +30,7 @@ function SignIn() {
 } 
 
 if(isset($_POST['submit'])) { 
-	SignIn(); 
+	SignIn($con); 
 } 
 
 ?>
--- Iowa Airlines - Front/script.js
+++ Iowa Airlines - Front/script.js
@@ -3,6 +3,7 @@ document.getElementById("foot01").innerHTML =
 
 document.getElementById("nav01").innerHTML = "<ul id='menu'>" + "<li><a href='index.html'>Home</a></li>" + "<li><a href='about.html'>About Us</a></li>" + "<li><a href='map.html'>Map</a></li>" + "<li><a href='plan.html'>Book a Trip</a></li>" + "</ul>";
 
+/*
 function login_validation() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -26,12 +27,12 @@ function find_user(){
 // just gather data from user -- java will do the logic 
 // next meeting - cost estimation (come prepared with documents)
 
-/*STEP 1: Use the functions username_okay and password_okay to check whether the username and password respect the
+STEP 1: Use the functions username_okay and password_okay to check whether the username and password respect the
           constraints
   STEP 2: Use the find_user function to obtain a node to the linked list 
           If the node returned by find_user is not null that means the user already exists
 	      In which case print the error message "Error: User already exists\n" and return ERROR  
-*/
+
 function register_user() {
     
 }
@@ -43,6 +44,6 @@ function change_user_password(){
 //redirect function (map --> book trip?)
 function load(url){
     location.href = url;
-}
+}*/
 
 
