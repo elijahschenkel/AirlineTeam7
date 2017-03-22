d13a748134f2cb9a82833e0368e72554f7ca5e4d
rjhemmingsen <robert-hemmingsen@uiowa.edu>
2017-03-22 00:07:28 -0500

    Fixing Git

--- AirlineTeam7/Iowa Airlines - Front/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("foot01").innerHTML =
-    '<p>&copy;  ' + new Date().getFullYear() + ' FSE - Team Seven. All rights reserved.</p>';
-
-document.getElementById("navLogin").innerHTML = "<ul id='login'>" + "<li><a href='login.html'>Log In</a></li>" + "</ul>";
-
-document.getElementById("nav01").innerHTML = "<ul id='menu'>" + "<li><a href='index.html'>Home</a></li>" + "<li><a href='about.html'>About Us</a></li>" + "<li><a href='book.html'>Book Flight</a></li>" + "<li><a href='create.html'>Create an Account</a></li>" + "</ul>";
