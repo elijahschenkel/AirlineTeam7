660036fab2846d2fac8e55d10da6822033f1ad6b
Kristine Holland <holland.krissy@gmail.com>
2017-04-17 18:39:14 -0500

    Removed unused code

--- Iowa Airlines - Front/assets/js/main.js
+++ Iowa Airlines - Front/assets/js/main.js
@@ -29,28 +29,6 @@
 			});
 			$('form').placeholder();
 
-//		// Fix: Flexbox min-height bug on IE.
-//			if (skel.vars.IEVersion < 12) {
-//
-//				var flexboxFixTimeoutId;
-//
-//				$window.on('resize.flexbox-fix', function() {
-//
-//					clearTimeout(flexboxFixTimeoutId);
-//
-//					flexboxFixTimeoutId = setTimeout(function() {
-//
-//						if ($wrapper.prop('scrollHeight') > $window.height())
-//							$wrapper.css('height', 'auto');
-//						else
-//							$wrapper.css('height', '100vh');
-//
-//					}, 250);
-//
-//				}).triggerHandler('resize.flexbox-fix');
-//
-//			}
-
 		// Nav.
 			var $nav = $header.children('nav'),
 				$nav_li = $nav.find('li');
